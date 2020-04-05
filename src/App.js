4523b040d5d5b0cf6a63a0952f3bb9d45c1817e9
amin havasi <pau.ahq@gmail.com>
2020-04-06 02:31:17 +0430

    ls

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const App = () => {
+    return (
+        <div>
+            <p className="btn-anim">salam</p>
+        </div>
+    );
+};
+
+export default App;
